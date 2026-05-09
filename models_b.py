bfc130c76f87994370295239a9bab38ad5f7189f
Faris Ćosić <faris.cosic@fet.ba>
2026-05-09 14:37:53 +0200

    Kreiran create/update model za country

--- models_b.py
+++ models_b.py
@@ -11,3 +11,17 @@ class Country(SQLModel, table=True):
     fifa_ranking: int
     continent: str
     world_cup_wins: int
+    
+class CountryCreate(SQLModel):
+    name: str
+    group: str  
+    fifa_ranking: int
+    continent: str
+    world_cup_wins: int
+
+class CountryUpate(SQLModel):
+    name: Optional[str] = None
+    group: Optional[str] = None  
+    fifa_ranking: Optional[int] = None
+    continent: Optional[str] = None
+    world_cup_wins: Optional[int] = None
