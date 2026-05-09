62a628f3a17cea5c23a9dfdb463c14f5852170c6
Faris Ćosić <faris.cosic@fet.ba>
2026-05-09 14:53:22 +0200

    Dodan get metod sa ID

--- routes_b.py
+++ routes_b.py
@@ -11,3 +11,11 @@ router = APIRouter(prefix="/country", tags=["Country"])
 def get_countries(session: Session = Depends(get_session)):
     countries = session.exec(select(Country)).all()
     return countries
+
+@router.get("/{country_id}")
+def get_country(country_id: int, session: Session = Depends(get_session)):
+    country = session.get(Country, country_id)
+    if not country:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Country not found")
+    return country
+
