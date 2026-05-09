484b42b2a57ae36901a64fc338565b4d0742199e
Faris Ćosić <faris.cosic@fet.ba>
2026-05-09 14:57:22 +0200

    Dodan put metod

--- routes_b.py
+++ routes_b.py
@@ -26,3 +26,18 @@ def create_country(country: CountryCreate, session: Session = Depends(get_sessio
     session.commit()
     session.refresh(new_country)
     return new_country
+
+@router.put("/{country_id}")
+def update_country(country_id: int, country_update: CountryUpate, session: Session = Depends(get_session)):
+    country = session.get(Country, country_id)
+    if not country:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Country not found")
+    
+    country_data = country_update.dict(exclude_unset=True)
+    for key, value in country_data.items():
+        setattr(country, key, value)
+    
+    session.add(country)
+    session.commit()
+    session.refresh(country)
+    return country
