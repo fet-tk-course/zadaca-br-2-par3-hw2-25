47dc3b94bdad580ef2dfd1ad0c4953e888242a34
Faris Ćosić <faris.cosic@fet.ba>
2026-05-09 14:36:59 +0200

    Kreiran model za country

--- models_b.py
+++ models_b.py
@@ -3,3 +3,11 @@ from typing import Optional
 
 # TODO: Student B - Definiši svoj SQLModel entitet ovdje
 # 
+
+class Country(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    group: str  
+    fifa_ranking: int
+    continent: str
+    world_cup_wins: int
